8697cc23aff95167a1eb52add98de16ee9711a31
Raymond <raymondnhim89@gmail.com>
2023-04-18 12:47:39 -0700

    updating nav

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//RUN ON DOCUMENT READY
+$(document).ready(function(){
+
+});
+
+//RUN ON PAGE LOADED
+$(window).on('load',function(){
+
+});
+
+//RUN WHEN WINDOW IS RESIZED
+$(window).on('resize',function(){
+
+});
+
+//RUN WHEN PAGE IS SCROLLED
+//THIS INCLUDES SCROLLING OF ANY KIND (NOT JUST USER CONTROLLED)
+$(window).on('scroll',function(){
+    
+});
