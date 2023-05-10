43dcfaf545f827eb40b5ae83f612413b472ec191
Raymond <raymondnhim89@gmail.com>
2023-05-10 12:36:10 -0700

    working on fixing nav, worked on section focus

--- js/component.js
+++ js/component.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     accordionMenu();
+    pageSelection();
+    anchorFocus();
 });
 
 function accordionMenu() {
@@ -12,4 +14,31 @@ function accordionMenu() {
         $('.accordion-text').addClass('hide-menu');
         $('.accordion-icon').removeClass('hide-menu');
     })
+}
+
+function pageSelection() {
+    $('#main').fadeIn(1000);
+
+    $('.toggle-action').not('#main').hide();
+
+    $('.toggle-section').click(function() {
+        // Get the ID of the div to toggle
+        var divId = $(this).attr('href'); 
+    
+        // Hide all toggleable divs except for the one to toggle
+        $('.toggle-action').not(divId).hide();
+    
+        // Fade in the toggleable div
+        $(divId).fadeIn(1000);
+      });
+}
+
+function anchorFocus() {
+    $('#home-toggle').addClass('nav-focus');
+  
+    $('.toggle-section').on('click', function() {
+      $('.toggle-section i, .toggle-section p').removeClass('nav-focus');
+      
+      $(this).find('i, p').addClass('nav-focus');
+    });
 }
