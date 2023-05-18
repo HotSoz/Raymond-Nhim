88ff96274a61b919657a8f110e0abf69fc3361b9
Raymond <raymondnhim89@gmail.com>
2023-05-17 19:10:18 -0700

    update for the day

--- js/cards-data.js
+++ js/cards-data.js
@@ -11,7 +11,8 @@ let Cards = [
         text: 'This project involves building a website from scratch for studying and project development purposes, with a focus on hands-on learning in HTML, CSS, JavaScript, project management, and collaboration. The project aims to develop skills for future web development projects and showcase abilities to potential employers or clients. This project also features Death Stranding. All rights go to Kojima Productions.',
         link: 'https://deconile.github.io/death-stranding/',
         techs: ['VSCode'],
-        codes: ['HTML', 'CSS', 'JavaScript', 'JQuery']
+        codes: ['HTML', 'CSS', 'JQuery'],
+        type: 'website',
     },
     {
         icon: 'fa-gamepad',
@@ -19,23 +20,25 @@ let Cards = [
         image: 'solar-clicker-img.png',
         text: 'A standard cookie clicker like game with a little twist that involves solar power instead of just cookies. Dive into the wonders of solar energy and many different planets to click. Learn something new with tips or fun facts about the vast universe. Welcome to Solar Clicker',
         link: '',
-        techs: ['Unity', "Aesprite"],
-        codes: ['C#']
+        techs: ['Unity', 'Aesprite'],
+        codes: ['C#'],
+        type: 'blog',
     },
     {
-        icon: 'fa-gamepad',
-        title: 'Solar Clicker',
-        image: 'solar-clicker-img.png',
-        text: 'A standard cookie clicker like game with a little twist that involves solar power instead of just cookies. Dive into the wonders of solar energy and many different planets to click. Learn something new with tips or fun facts about the vast universe. Welcome to Solar Clicker',
-        link: '',
-        techs: ['Unity', "Aesprite"],
-        codes: ['C#']
+          icon: 'fa-code',
+        title: 'Card Transition',
+        image: 'card-transition.png',
+        text: 'Want to pick up a card as you hover it?',
+        link: 'https://codepen.io/HotSoz/pen/LYggbZq',
+        techs: ['CodePen'],
+        codes: ['HTML', 'CSS'],
+        type: 'project',
     }
 ];
 
 function CardShowCase() {
     const card1 = Cards.map((obj, index) => {
-        const { icon, title, image, text, link, techs, codes } = obj;
+        const { icon, title, image, text, link, techs, codes, type } = obj;
         let cardTech = "";
         let cardCode = "";
         techs.forEach((tech) => {
@@ -64,9 +67,11 @@ function CardShowCase() {
                         </ul>
                     </div>
                 </div>
-                <div>
-                    <p><a class="card" href="${link}" target="_blank">View Project</a></p>
-                </div>
+                <a href="${link}" target="_blank">
+                    <div class="card-link">
+                        <p>view ${type}</p>
+                    </div>
+                </a>
             </div>
         `);
     })
