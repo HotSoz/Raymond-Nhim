b9dd93a0d390425c728c043abc844eec48a69e16
Raymond <raymondnhim89@gmail.com>
2023-05-24 19:01:04 -0700

    updating for the day

--- js/event-handler.js
+++ js/event-handler.js
@@ -1,26 +1,40 @@
 $(document).ready(function() {
     themeChange();
+    themeSVGChange()
 });
 
 function themeChange() {
-    $('#toggle-theme').click(function() {
-        var currentTheme = $('html').attr('data-theme');
-        
-        switch (currentTheme) {
-            case 'dark':
-              newTheme = 'light';
-              break;
-            case 'light':
-              newTheme = 'purple';
-              break;
-            case 'purple':
-              newTheme = 'dark';
-              break;
-            default:
-              newTheme = 'dark';
-              break;
-          }
+  var themes = ['dark', 'light', 'purple'];
+  var currentThemeIndex = themes.indexOf($('html').attr('data-theme'));
+  
+  $('#toggle-theme').click(function() {
+    currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+    var newTheme = themes[currentThemeIndex];
+    $('html').attr('data-theme', newTheme);
+  });
+}
 
-        $('html').attr('data-theme', newTheme);
-    });
+function themeSVGChange() {
+  $('#toggle-theme').click(function() {
+    var currentTheme = $('html').attr('data-theme');
+    var sunGroup = $('#sun-group');
+    var moonGroup = $('#moon-group');
+    var cloudGroup = $('#cloud-group');
+    var glowGroup = $('#backlight-group');
+    
+    // Hide all groups
+    sunGroup.removeClass('show').addClass('hide');
+    moonGroup.removeClass('show').addClass('hide');
+    cloudGroup.removeClass('show').addClass('hide');
+    glowGroup.removeClass('show').addClass('hide');
+    
+    // Show specific groups based on the theme
+    if (currentTheme === 'dark' || currentTheme === 'purple') {
+      moonGroup.removeClass('hide').addClass('show');
+      glowGroup.removeClass('hide').addClass('show');
+    } else if (currentTheme === 'light') {
+      sunGroup.removeClass('hide').addClass('show');
+      cloudGroup.removeClass('hide').addClass('show');
+    }
+  });
 }
